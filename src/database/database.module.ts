6085c680b5857655fc3d4880e9055b420e707526
Carlos Roberto <carlosrbta@gmail.com>
2026-09-10 15:02:17 -0300

    Auto commit

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -11,18 +11,32 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        port: config.get<number>('DATABASE_PORT', { infer: true }),
-        username: config.get<string>('DATABASE_USER', { infer: true }),
-        password: config.get<string>('DATABASE_PASSWORD', { infer: true }),
-        database: config.get<string>('DATABASE_NAME', { infer: true }),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        migrations: [__dirname + '/migrations/*{.ts,.js}'],
-        migrationsRun: true,
-        synchronize: false,
-        logging: false,
-      }),
+      useFactory: (config: ConfigService) => {
+        console.log({
+          type: 'postgres',
+          port: config.get<number>('DATABASE_PORT', { infer: true }),
+          username: config.get<string>('DATABASE_USER', { infer: true }),
+          password: config.get<string>('DATABASE_PASSWORD', { infer: true }),
+          database: config.get<string>('DATABASE_NAME', { infer: true }),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/migrations/*{.ts,.js}'],
+          migrationsRun: true,
+          synchronize: false,
+          logging: false,
+        });
+        return {
+          type: 'postgres',
+          port: config.get<number>('DATABASE_PORT', { infer: true }),
+          username: config.get<string>('DATABASE_USER', { infer: true }),
+          password: config.get<string>('DATABASE_PASSWORD', { infer: true }),
+          database: config.get<string>('DATABASE_NAME', { infer: true }),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          migrations: [__dirname + '/migrations/*{.ts,.js}'],
+          migrationsRun: true,
+          synchronize: false,
+          logging: false,
+        };
+      },
     }),
   ],
 })
