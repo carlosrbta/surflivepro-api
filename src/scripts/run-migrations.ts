7a9ed7b0d3bc1c12f817a20053d37c45d18b3ef9
Carlos Roberto <carlosrbta@gmail.com>
2026-09-10 14:59:38 -0300

    Auto commit

--- src/scripts/run-migrations.ts
+++ src/scripts/run-migrations.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT || '5432', 10),
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 });
 
 async function runMigrations() {
